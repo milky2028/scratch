5b6ec1c622fed19901efb388dcabb04f6bd4a8bc
Tyler <tgross@skyroad.com>
2019-03-27 10:23:57 -0500

    base js

---
+++ app.js
@@ -0,0 +1,37 @@
+const iterations = 50;
+const multiplier = 1000000000;
+
+function calculatePrimes(iterations, multiplier) {
+  var primes = [];
+  for (var i = 0; i < iterations; i++) {
+    var candidate = i * (multiplier * Math.random());
+    var isPrime = true;
+    for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+      if (candidate % c === 0) {
+        // not prime
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(candidate);
+    }
+  }
+  return primes;
+}
+
+function doPointlessComputationsWithBlocking() {
+  var primes = calculatePrimes(iterations, multiplier);
+  console.log(primes);
+}
+
+// function shapeShiftBox() {
+//   const box = document.querySelector('#rect');
+//   box.style.height = Math.floor(Math.random() * 700);
+// }
+
+// setTimeout(() => {
+//   shapeShiftBox();
+// }, 5000);
+
+// doPointlessComputationsWithBlocking();
