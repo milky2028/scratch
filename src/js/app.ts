9af29e156572dd11aaeda05a484ac5e4899ade35
Tyler <tgross@skyroad.com>
2019-03-27 15:03:12 -0500

    most setup done

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,12 +1,21 @@
+declare const Vue: any;
+
+const v = new Vue({
+  el: '#app',
+  data: {
+    boxes: 100
+  }
+});
+
 const iterations = 50;
-const multiplier = 1000000000;
+const multiplier = 100000000000;
 
 function calculatePrimes(iterations: number, multiplier: number) {
-  var primes = [];
-  for (var i = 0; i < iterations; i++) {
-    var candidate = i * (multiplier * Math.random());
-    var isPrime = true;
-    for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+  const primes = [];
+  for (let i = 0; i < iterations; i++) {
+    const candidate = i * (multiplier * Math.random());
+    let isPrime = true;
+    for (let c = 2; c <= Math.sqrt(candidate); ++c) {
       if (candidate % c === 0) {
         // not prime
         isPrime = false;
@@ -20,18 +29,39 @@ function calculatePrimes(iterations: number, multiplier: number) {
   return primes;
 }
 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function doPointlessComputationsWithBlocking() {
-  var primes = calculatePrimes(iterations, multiplier);
+  const primes = calculatePrimes(iterations, multiplier);
   console.log(primes);
 }
 
-// function shapeShiftBox() {
-//   const box = <HTMLElement>document.querySelector('#box');
-//   box.style.height = `${Math.floor(Math.random() * 700)}px`;
-// }
+function getRandomNumber(windowMeasurement: number) {
+  const randomNumber = Math.floor(Math.random() * (windowMeasurement / 10));
+  return (randomNumber > windowMeasurement / 10) ? windowMeasurement / 10 : randomNumber;
+}
 
-// setTimeout(() => {
-//   shapeShiftBox();
-// }, 5000);
+function shapeShiftBoxes() {
+  const boxes = <NodeListOf<HTMLElement>>document.querySelectorAll('.box');
+  boxes.forEach((box) => {
+    const s = box.style;
+    s.height = `${getRandomNumber(window.innerHeight)}px`;
+    s.width = `${getRandomNumber(window.innerWidth)}px`;
+    s.backgroundColor = getRandomColor();
+    s.borderRadius = `${getRandomNumber(window.innerWidth)}px`;
+  });
+}
 
-// doPointlessComputationsWithBlocking();
+shapeShiftBoxes();
+setInterval(() => {
+  // doPointlessComputationsWithBlocking();
+  // window.requestAnimationFrame(shapeShiftBoxes);
+  shapeShiftBoxes();
+}, 500);
