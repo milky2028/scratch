0b2d79139be60ed0531ceaec6e836e971c0b2414
Tyler <tgross@skyroad.com>
2019-03-27 15:08:24 -0500

    computation in worker

--- src/js/app.ts
+++ src/js/app.ts
@@ -7,28 +7,6 @@ const v = new Vue({
   }
 });
 
-const iterations = 50;
-const multiplier = 100000000000;
-
-function calculatePrimes(iterations: number, multiplier: number) {
-  const primes = [];
-  for (let i = 0; i < iterations; i++) {
-    const candidate = i * (multiplier * Math.random());
-    let isPrime = true;
-    for (let c = 2; c <= Math.sqrt(candidate); ++c) {
-      if (candidate % c === 0) {
-        // not prime
-        isPrime = false;
-        break;
-      }
-    }
-    if (isPrime) {
-      primes.push(candidate);
-    }
-  }
-  return primes;
-}
-
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
   let color = '#';
@@ -38,18 +16,13 @@ function getRandomColor() {
   return color;
 }
 
-function doPointlessComputationsWithBlocking() {
-  const primes = calculatePrimes(iterations, multiplier);
-  console.log(primes);
-}
-
 function getRandomNumber(windowMeasurement: number) {
   const randomNumber = Math.floor(Math.random() * (windowMeasurement / 10));
   return (randomNumber > windowMeasurement / 10) ? windowMeasurement / 10 : randomNumber;
 }
 
 function shapeShiftBoxes() {
-  const boxes = <NodeListOf<HTMLElement>>document.querySelectorAll('.box');
+  const boxes = document.querySelectorAll('.box') as NodeListOf<HTMLElement>;
   boxes.forEach((box) => {
     const s = box.style;
     s.height = `${getRandomNumber(window.innerHeight)}px`;
---
+++ src/js/worker.ts
@@ -0,0 +1,23 @@
+function calculatePrimes(iterations: number, multiplier: number) {
+  const primes = [];
+  for (let i = 0; i < iterations; i++) {
+    const candidate = i * (multiplier * Math.random());
+    let isPrime = true;
+    for (let c = 2; c <= Math.sqrt(candidate); ++c) {
+      if (candidate % c === 0) {
+        // not prime
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(candidate);
+    }
+  }
+  return primes;
+}
+
+function doPointlessComputationsWithBlocking() {
+  const primes = calculatePrimes(50, 100000000000);
+  console.log(primes);
+}
